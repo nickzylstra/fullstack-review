93fbd1f55062879a3c4617a6a977146bf4f75645
nickzylstra <nick.zylstra@gmail.com>
2019-11-22 14:27:32 -0800

    Updates for save func prep

--- database/index.js
+++ database/index.js
@@ -12,7 +12,7 @@ const repoSchema = mongoose.Schema({
 
 const Repo = mongoose.model('Repo', repoSchema);
 
-const save = (/* TODO */) => {
+const save = (repos, next) => {
   // TODO: Your code here
   // This function should save a repo or repos to
   // the MongoDB
--- server/controllers.js
+++ server/controllers.js
@@ -1,11 +1,22 @@
+/* eslint-disable no-console */
 const db = require('../database/index');
 const { getReposByUsername } = require('../helpers/github');
 
 module.exports = {
   addUser: (username, next) => {
-    getReposByUsername(username, (err, data) => {
-      debugger;
-      db.save(data);
+    getReposByUsername(username, (err, { body }) => {
+      if (err) {
+        console.log(err);
+        next();
+      }
+      const repos = body;
+      db.save(repos, (error, data) => {
+        if (error) {
+          console.log(error);
+        }
+        // TODO update below
+        next(data);
+      });
     });
   },
 };
