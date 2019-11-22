69b761be961bda617a4ec866255896400e6a2f03
nickzylstra <nick.zylstra@gmail.com>
2019-11-22 12:18:26 -0800

    Updates github helper

--- helpers/github.js
+++ helpers/github.js
@@ -1,20 +1,22 @@
 const request = require('request');
 const config = require('../config.js');
 
-let getReposByUsername = (/* TODO */) => {
+const getReposByUsername = (username, next) => {
   // TODO - Use the request module to request repos for a specific
   // user from the github API
 
-  // The options object has been provided to help you out, 
+  // The options object has been provided to help you out,
   // but you'll have to fill in the URL
-  let options = {
-    url: 'FILL ME IN',
+  const options = {
+    url: `https://api.github.com/${username}/repos`,
     headers: {
       'User-Agent': 'request',
-      'Authorization': `token ${config.TOKEN}`
-    }
+      Authorization: `token ${config.TOKEN}`,
+      Accept: 'application/vnd.github.v3+json',
+    },
   };
 
-}
+  return request(options, next);
+};
 
 module.exports.getReposByUsername = getReposByUsername;
