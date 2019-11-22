d40466366ebb6274dd45f80edfdfef9782456a48
nickzylstra <nick.zylstra@gmail.com>
2019-11-22 14:50:22 -0800

    Sets up database entry

--- database/index.js
+++ database/index.js
@@ -4,7 +4,7 @@ mongoose.connect('mongodb://localhost/fetcher');
 
 const repoSchema = mongoose.Schema({
   repoId: Number,
-  views: Number,
+  size: Number,
   url: String,
   ownerLogin: String,
   ownerId: Number,
@@ -13,9 +13,20 @@ const repoSchema = mongoose.Schema({
 const Repo = mongoose.model('Repo', repoSchema);
 
 const save = (repos, next) => {
+  debugger;
   // TODO: Your code here
   // This function should save a repo or repos to
   // the MongoDB
+
+  repos.forEach(({ id, owner, url, size }) => {
+    const curRepo = new Repo({
+      repoId: id,
+      size,
+      url,
+      ownerLogin: owner.login,
+      ownerId: owner.id,
+    });
+  });
 };
 
 module.exports.save = save;
